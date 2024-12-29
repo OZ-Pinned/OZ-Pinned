c9cdb5e70057217a21f2ce666cde77b0e74fdaeb
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2024-12-29 22:26:16 +0900

    ⚡️ feat : 슬라이더 함수 추가, 스타일 적용

--- pinned/lib/emotion.dart
+++ pinned/lib/emotion.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -20,36 +22,116 @@ class EmotionPage extends StatefulWidget {
 
 class _EmotionPageState extends State<EmotionPage> {
   double currentValue = 0.0;
+
+  String getImagePath(double value) {
+    if (value <= 25) {
+      return 'assets/images/angryEmotion.png'; // 슬라이더 값이 0~25일 때
+    } else if (value <= 50) {
+      return 'assets/images/sadEmotion.png'; // 슬라이더 값이 26~50일 때
+    } else if (value <= 75) {
+      return 'assets/images/noneEmotion.png'; // 슬라이더 값이 51~75일 때
+    } else {
+      return 'assets/images/happyEmotion.png'; // 슬라이더 값이 76~100일 때
+    }
+  }
+
+  Color getColorCode(double value) {
+    if (value <= 25) {
+      return Color(0xff96BDFF); // 슬라이더 값이 0~25일 때
+    } else if (value <= 50) {
+      return Color(0xff59F09F); // 슬라이더 값이 26~50일 때
+    } else if (value <= 75) {
+      return Color(0xffFFD751); // 슬라이더 값이 51~75일 때
+    } else {
+      return Color(0xffFFAE51); // 슬라이더 값이 76~100일 때
+    }
+  }
+
+  Color getOuterCircleColorCode(double value) {
+    if (value <= 25) {
+      return Color(0xff6499F9); // 슬라이더 값이 0~25일 때
+    } else if (value <= 50) {
+      return Color(0xff1BDC74); // 슬라이더 값이 26~50일 때
+    } else if (value <= 75) {
+      return Color(0xffF2BC02); // 슬라이더 값이 51~75일 때
+    } else {
+      return Color(0xffF28202); // 슬라이더 값이 76~100일 때
+    }
+  }
+
+  Color getInnerCircleColorCode(double value) {
+    if (value <= 25) {
+      return Color(0xffCFDDF7); // 슬라이더 값이 0~25일 때
+    } else if (value <= 50) {
+      return Color(0xff9BF9CD); // 슬라이더 값이 26~50일 때
+    } else if (value <= 75) {
+      return Color(0xffFAEA9B); // 슬라이더 값이 51~75일 때
+    } else {
+      return Color(0xffFAD19B); // 슬라이더 값이 76~100일 때
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text('emotionPage'),
       ),
-      backgroundColor: Color(0xff96BDFF),
+      backgroundColor: getColorCode(currentValue),
       body: Column(
         children: [
+          SizedBox(
+            height: 75,
+          ),
           Text(
             '오늘의 기분은 어떤가요?',
             style: TextStyle(
                 fontFamily: 'LeeSeoYun', color: Colors.white, fontSize: 26),
           ),
-          Image.asset('assets/images/Kangeroo.png'),
+          SizedBox(
+            height: 93,
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: AnimatedSwitcher(
+              duration: Duration(milliseconds: 300), // 애니메이션 속도 설정
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return FadeTransition(
+                    opacity: animation, child: child); // 페이드 효과
+              },
+              child: SizedBox(
+                key: ValueKey<double>(
+                    currentValue), // 슬라이더 값에 따라 다른 키를 설정하여 애니메이션 적용
+                height: 181,
+                width: 300,
+                child: Image.asset(
+                  getImagePath(currentValue),
+                  fit: BoxFit.contain, // 이미지를 중앙에 맞춰줍니다.
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 138,
+          ),
           SliderTheme(
               data: SliderTheme.of(context).copyWith(
                 activeTrackColor: Color(0xffFFF2F0), // 슬라이더의 활성화된 트랙 색상
                 inactiveTrackColor: Color(0xffFFF2F0), // 비활성화된 트랙 색상
                 trackHeight: 17.0, // 트랙 두께
                 thumbColor: Color(0xff6499F9), // 슬라이더의 썸(핸들) 색상
-                thumbShape: CustomSliderThumbCircle(),
+                thumbShape: CustomSliderThumbCircle(
+                    thumbColor: getOuterCircleColorCode(currentValue),
+                    innerCircleColor: getInnerCircleColorCode(currentValue)),
                 overlayColor:
                     Colors.blue.withOpacity(0.2), // 썸 클릭 시 나오는 오버레이 색상
                 overlayShape:
                     RoundSliderOverlayShape(overlayRadius: 20.0), // 오버레이 크기
                 tickMarkShape: RoundSliderTickMarkShape(), // 트랙 위의 점 표시 스타일
-                activeTickMarkColor: Color(0xff6499F9), // 활성화된 점 색상
-                inactiveTickMarkColor: Color(0xff6499F9), // 비활성화된 점 색상
+                activeTickMarkColor:
+                    getOuterCircleColorCode(currentValue), // 활성화된 점 색상
+                inactiveTickMarkColor:
+                    getOuterCircleColorCode(currentValue), // 비활성화된 점 색상
               ),
               child: Slider(
                 value: currentValue,
@@ -59,6 +141,88 @@ class _EmotionPageState extends State<EmotionPage> {
                   currentValue = value;
                 }),
               )),
+          SizedBox(
+            height: 78,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    content: Container(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "지난 2주 이내의 감정을\n바탕으로 선택해주세요!",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 25,
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xffFF516A),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              fixedSize: Size(201, 31),
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text(
+                              '확인',
+                              style:
+                                  TextStyle(fontSize: 13, color: Colors.white),
+                            ),
+                          ), // 버튼 내 텍스트 간격
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text(
+                              "다시 보지 않기",
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              minimumSize: Size(320, 48),
+            ),
+            child: Text(
+              '다음',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -66,6 +230,14 @@ class _EmotionPageState extends State<EmotionPage> {
 }
 
 class CustomSliderThumbCircle extends SliderComponentShape {
+  final Color thumbColor;
+  final Color innerCircleColor;
+
+  CustomSliderThumbCircle({
+    required this.thumbColor,
+    required this.innerCircleColor,
+  });
+
   @override
   Size getPreferredSize(bool isEnabled, bool isDiscrete) {
     return Size(30.0, 30.0); // thumb 크기 설정
@@ -88,16 +260,16 @@ class CustomSliderThumbCircle extends SliderComponentShape {
   }) {
     final Canvas canvas = context.canvas;
 
-    // 바깥 원
+    // 바깥 원 (thumb)
     final Paint outerCirclePaint = Paint()
-      ..color = Color(0xff6499F9) // 바깥 원 색상
+      ..color = thumbColor // 전달받은 색상 사용
       ..style = PaintingStyle.fill;
-    canvas.drawCircle(center, 15.0, outerCirclePaint); // 바깥 원의 반지름: 15.0
+    canvas.drawCircle(center, 15.0, outerCirclePaint); // 바깥 원의 반지름
 
     // 안쪽 원
     final Paint innerCirclePaint = Paint()
-      ..color = Color(0xffCFDDF7) // 안쪽 원 색상
+      ..color = innerCircleColor // 전달받은 색상 사용
       ..style = PaintingStyle.fill;
-    canvas.drawCircle(center, 7.0, innerCirclePaint); // 안쪽 원의 반지름: 7.0
+    canvas.drawCircle(center, 7.0, innerCirclePaint); // 안쪽 원의 반지름
   }
 }
