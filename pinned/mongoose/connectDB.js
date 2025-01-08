a69c77ea6dde6955aec3783f78d478bdba65aadd
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-01-08 11:05:06 +1100

    🌱 add : 몽고 서버 연결

--- pinned/mongoose/connectDB.js
+++ pinned/mongoose/connectDB.js
@@ -1,25 +1,28 @@
-const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config();
+
+const client = new MongoClient(process.env.uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 const connectDB = async () => {
-    try {
-        mongoose.set("debug", true);
+  try {
+    // 클라이언트 연결
+    await client.connect();
 
-        await mongoose.connect("mongodb://root:591006@localhost:27017/admin", {
-            dbName: "userDB",
-        });
-        console.log("MongoDB 연결 성공");
-    } catch (err) {
-        console.error("MongoDB 연결 실패:", err);
-    }
+    // Ping 명령어로 연결 확인
+    await client.db("admin").command({ ping: 1 });
+    console.log("MongoDB 연결 성공!");
+  } catch (err) {
+    console.error("MongoDB 연결 실패:", err);
+  } finally {
+    // 필요할 경우 연결 종료
+    await client.close();
+  }
 };
-
-mongoose.connection.on("error", (err) => {
-    console.error("MongoDB 연결 에러:", err);
-});
-
-mongoose.connection.on("disconnected", () => {
-    console.error("MongoDB 연결 해제. 연결 재시도 중...");
-    connectDB(); // 재시도
-});
 
 module.exports = connectDB;
