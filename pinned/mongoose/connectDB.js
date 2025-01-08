fbb7c3e80d82642f24701d4701415a5096b446bb
hyewon4052 <s2311@e-mirim.hs.kr>
2025-01-08 13:46:34 +1100

    init commit

--- pinned/mongoose/connectDB.js
+++ pinned/mongoose/connectDB.js
@@ -1,7 +1,9 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require('dotenv').config();
 
-const client = new MongoClient(process.env.uri, {
+const uri = "mongodb+srv://pinned:pinneddatabase123@pinned.zae3y.mongodb.net/pinned?retryWrites=true&w=majority&appName=pinned"
+
+const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -15,7 +17,21 @@ const connectDB = async () => {
     await client.connect();
 
     // Ping 명령어로 연결 확인
-    await client.db("admin").command({ ping: 1 });
+    const database = client.db("pinned");  // 데이터베이스 선택, 없으면 자동 생성
+    const users = database.collection("userDB");  // 컬렉션 선택, 없으면 자동 생성
+
+    // 새로운 사용자 데이터
+    const newUser = {
+      id: 2,
+      email: "user@example.com",
+      name: "홍길동",
+      character: 0
+    };
+
+    // 데이터 추가
+    const result = await users.insertOne(newUser);
+    console.log(`New user added with the following id: ${result.insertedId}`);
+
     console.log("MongoDB 연결 성공!");
   } catch (err) {
     console.error("MongoDB 연결 실패:", err);
