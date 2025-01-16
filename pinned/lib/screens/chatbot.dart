b66385df2f12134843226c97db4616dd2d310dfe
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-01-16 22:51:20 +1100

    ✅ fix : 챗봇 페이지

--- pinned/lib/screens/chatbot.dart
+++ pinned/lib/screens/chatbot.dart
@@ -18,6 +18,7 @@ class _ChatBotPageState extends State<ChatBot> {
   List<Map<String, String>> chatedMessage = [];
 
   Future<String?> _handleSubmitted() async {
+    print(inputedMessage);
     try {
       var response = await http.post(
         Uri.parse("http://localhost:3000/chatbot/get"),
@@ -35,18 +36,57 @@ class _ChatBotPageState extends State<ChatBot> {
       final data = json.decode(response.body);
 
       if (response.statusCode == 201) {
-        print(data['res']);
+        print("data['res'], $inputedMessage");
         chatedMessage.add({'User': inputedMessage});
         chatedMessage.add({'AI': data['res']});
 
-        setState(() {
-          inputedMessage = "";
-        });
+        print(chatedMessage);
+
+        inputedMessage = "";
       } else {
         print('Failed to upload diary: ${response.body}');
       }
     } catch (error) {
       print('ai error : $error');
+    }
+
+    return null;
+  }
+
+  Future<String?> handleSubmitted() async {
+    print(inputedMessage);
+    try {
+      var response = await http.post(
+        Uri.parse("http://localhost:3000/chatbot/get"),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: jsonEncode(
+          {
+            'msg': inputedMessage,
+          },
+        ),
+      );
+
+      final data = json.decode(response.body);
+
+      if (response.statusCode == 201) {
+        print("data['res'], $inputedMessage");
+
+        // AI의 응답과 유저의 메시지를 리스트에 추가
+        setState(() {
+          chatedMessage.add({'User': inputedMessage});
+          chatedMessage.add({'AI': data['res']});
+        });
+
+        print(chatedMessage);
+        inputedMessage = ""; // 메시지 입력란 초기화
+      } else {
+        print('Failed to upload diary: ${response.body}');
+      }
+    } catch (error) {
+      print('AI error: $error');
     }
 
     return null;
@@ -136,21 +176,86 @@ class _ChatBotPageState extends State<ChatBot> {
                       alignment: isUserMessage
                           ? Alignment.centerRight
                           : Alignment.centerLeft,
-                      child: Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: isUserMessage
-                              ? Colors.blueAccent
-                              : Colors.grey[300],
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Text(
-                          message.values.first,
-                          style: TextStyle(
-                            color: isUserMessage ? Colors.white : Colors.black,
-                          ),
-                        ),
-                      ),
+                      child: isUserMessage
+                          ? Container(
+                              margin: EdgeInsets.only(left: 60),
+                              padding: EdgeInsets.only(
+                                top: 7.5,
+                                bottom: 7.5,
+                                left: 17,
+                                right: 17,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Color(0xffFF516A),
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(14),
+                                  topRight: Radius.circular(0),
+                                  bottomRight: Radius.circular(14),
+                                  bottomLeft: Radius.circular(14),
+                                ),
+                                border: Border.all(
+                                  color: Color(0xffDADADA),
+                                ),
+                              ),
+                              child: Text(
+                                message.values.first,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  height: 1.2,
+                                  color: Color(0xffFFFFFF),
+                                ),
+                              ),
+                            )
+                          : Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  spacing: 7,
+                                  children: [
+                                    SvgPicture.asset(
+                                      'assets/images/chatBotKoKo.svg',
+                                      width: 45,
+                                      height: 45,
+                                    ),
+                                    Text(
+                                      '코코',
+                                      style: TextStyle(
+                                        fontFamily: 'LeeSeoYun',
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(left: 60),
+                                  padding: EdgeInsets.only(
+                                    top: 7.5,
+                                    bottom: 7.5,
+                                    left: 17,
+                                    right: 17,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    color: Color(0xffFFFFFF),
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(0),
+                                      topRight: Radius.circular(14),
+                                      bottomRight: Radius.circular(14),
+                                      bottomLeft: Radius.circular(14),
+                                    ),
+                                    border: Border.all(
+                                      color: Color(0xffDADADA),
+                                    ),
+                                  ),
+                                  child: Text(
+                                    message.values.first,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      height: 1.2,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                     ),
                   );
                 },
@@ -171,7 +276,8 @@ class _ChatBotPageState extends State<ChatBot> {
                         ),
                         onChanged: (val) {
                           setState(() {
-                            inputedMessage = val;
+                            inputedMessage =
+                                val; // inputedMessage를 업데이트하면서 UI도 갱신
                           });
                         },
                       ),
@@ -184,8 +290,9 @@ class _ChatBotPageState extends State<ChatBot> {
                           width: 36,
                         ),
                         onPressed: () async {
-                          if (inputedMessage.trim().isNotEmpty) {
-                            await _handleSubmitted();
+                          if (inputedMessage.trim() != 0) {
+                            print('메시지 전송: $inputedMessage'); // 로그 추가
+                            await handleSubmitted();
                           }
                         },
                       ),
