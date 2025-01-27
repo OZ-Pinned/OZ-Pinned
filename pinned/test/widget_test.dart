b4e10c5b42867fcacfae6ac6ed58dd5d47bd1cc4
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-01-27 20:22:41 +0900

    수정

--- pinned/test/widget_test.dart
+++ pinned/test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:pinned/main.dart';
+import 'package:pinned/pages/main/selectPage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(SelectPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
