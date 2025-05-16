fcefda0e2ab800e479a01256c8062155acb300b9
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-05-16 19:58:54 +0900

    ip 주소 변경

--- pinned/server.js
+++ pinned/server.js
@@ -33,6 +33,6 @@ app.use("/home", homeRoutes);
 
 // 서버 포트 설정
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`서버가 http://localhost:${PORT}에서 실행 중입니다.`);
 });
